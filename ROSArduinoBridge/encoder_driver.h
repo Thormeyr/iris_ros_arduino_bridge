59647274de0e18ca2cf17d0c31e6e238f0e159dd
Pedro Thormeyr <84100284+Thormeyr@users.noreply.github.com>
2024-09-17 16:32:04 -0300

    refatorando driver encoder

--- ROSArduinoBridge/encoder_driver.h
+++ ROSArduinoBridge/encoder_driver.h
@@ -1,20 +1,22 @@
-/* *************************************************************
-   Encoder driver function definitions - by James Nugen
-   ************************************************************ */
-   
-   
-#ifdef ARDUINO_ENC_COUNTER
-  //below can be changed, but should be PORTD pins; 
-  //otherwise additional changes in the code are required
-  #define LEFT_ENC_PIN_A PD2  //pin 2
-  #define LEFT_ENC_PIN_B PD3  //pin 3
-  
-  //below can be changed, but should be PORTC pins
-  #define RIGHT_ENC_PIN_A PC4  //pin A4
-  #define RIGHT_ENC_PIN_B PC5   //pin A5
-#endif
-   
-long readEncoder(int i);
-void resetEncoder(int i);
-void resetEncoders();
+#ifndef ENCODER_DRIVER_H
+#define ENCODER_DRIVER_H
 
+// Include necessary libraries or headers
+#include <Arduino.h>
+
+// Constants for encoder configuration
+const unsigned long SPEED_TIMEOUT = 500000;   // Time used to determine wheel is not spinning
+const unsigned int UPDATE_TIME = 500;         // Time used to output serial data
+const double WHEEL_DIAMETER_CM = 16.5;           // Motor wheel diameter (centimeters)
+const double WHEEL_CIRCUMFERENCE_CM = 56.5;      // Motor wheel circumference (centimeters)
+
+// Pin Declarations
+const int PIN_SPEED_LEFT = 12;  // Change to the actual pin number
+const int PIN_SPEED_RIGHT = 13; // Change to the actual pin number
+
+// Function declarations
+void initEncoders();
+void resetEncoders();
+long readEncoder(int i);
+
+#endif
--- ROSArduinoBridge/encoder_driver.ino
+++ ROSArduinoBridge/encoder_driver.ino
@@ -1,82 +1,84 @@
-/* *************************************************************
-   Encoder definitions
-   
-   Add an "#ifdef" block to this file to include support for
-   a particular encoder board or library. Then add the appropriate
-   #define near the top of the main ROSArduinoBridge.ino file.
-   
-   ************************************************************ */
-   
-#ifdef USE_BASE
+#include "encoder_driver.h"
 
-#ifdef ROBOGAIA
-  /* The Robogaia Mega Encoder shield */
-  #include "MegaEncoderCounter.h"
+// Variables used in ReadSpeed function
+double _freqLeft;   // Frequency of the signal on the left speed pin
+double _rpmLeft;    // Left wheel speed in revolutions per minute
+double _mpsLeft;    // Left wheel speed in meters per second
 
-  /* Create the encoder shield object */
-  MegaEncoderCounter encoders = MegaEncoderCounter(4); // Initializes the Mega Encoder Counter in the 4X Count mode
-  
-  /* Wrap the encoder reading function */
-  long readEncoder(int i) {
-    if (i == LEFT) return encoders.YAxisGetCount();
-    else return encoders.XAxisGetCount();
-  }
+double _freqRight;  // Frequency of the signal on the right speed pin
+double _rpmRight;   // Right wheel speed in revolutions per minute
+double _mpsRight;   // Right wheel speed in meters per second
 
-  /* Wrap the encoder reset function */
-  void resetEncoder(int i) {
-    if (i == LEFT) return encoders.YAxisReset();
-    else return encoders.XAxisReset();
-  }
-#elif defined(ARDUINO_ENC_COUNTER)
-  volatile long left_enc_pos = 0L;
-  volatile long right_enc_pos = 0L;
-  static const int8_t ENC_STATES [] = {0,1,-1,0,-1,0,0,1,1,0,0,-1,0,-1,1,0};  //encoder lookup table
-    
-  /* Interrupt routine for LEFT encoder, taking care of actual counting */
-  ISR (PCINT2_vect){
-  	static uint8_t enc_last=0;
-        
-	enc_last <<=2; //shift previous state two places
-	enc_last |= (PIND & (3 << 2)) >> 2; //read the current state into lowest 2 bits
-  
-  	left_enc_pos += ENC_STATES[(enc_last & 0x0f)];
-  }
-  
-  /* Interrupt routine for RIGHT encoder, taking care of actual counting */
-  ISR (PCINT1_vect){
-        static uint8_t enc_last=0;
-          	
-	enc_last <<=2; //shift previous state two places
-	enc_last |= (PINC & (3 << 4)) >> 4; //read the current state into lowest 2 bits
-  
-  	right_enc_pos += ENC_STATES[(enc_last & 0x0f)];
-  }
-  
-  /* Wrap the encoder reading function */
-  long readEncoder(int i) {
-    if (i == LEFT) return left_enc_pos;
-    else return right_enc_pos;
-  }
-
-  /* Wrap the encoder reset function */
-  void resetEncoder(int i) {
-    if (i == LEFT){
-      left_enc_pos=0L;
-      return;
-    } else { 
-      right_enc_pos=0L;
-      return;
-    }
-  }
-#else
-  #error A encoder driver must be selected!
-#endif
-
-/* Wrap the encoder reset function */
-void resetEncoders() {
-  resetEncoder(LEFT);
-  resetEncoder(RIGHT);
+// Initialize encoder pins
+void initEncoders() {
+    pinMode(PIN_SPEED_LEFT, INPUT);
+    pinMode(PIN_SPEED_RIGHT, INPUT);
 }
 
-#endif
+// Read the speed from the input pin and calculate RPM and m/s
+void ReadSpeed() {
+    static bool lastStateLeft = false;  
+    static unsigned long last_uS_Left = 0;    
+    static unsigned long timeout_uS_Left = 0;  
 
+    static bool lastStateRight = false;  
+    static unsigned long last_uS_Right = 0;    
+    static unsigned long timeout_uS_Right = 0;  
+
+    bool stateLeft = digitalRead(PIN_SPEED_LEFT);
+    bool stateRight = digitalRead(PIN_SPEED_RIGHT);
+
+    if (stateLeft != lastStateLeft) {
+        unsigned long current_uS = micros();
+        unsigned long elapsed_uS = current_uS - last_uS_Left;
+
+        double period_uS = elapsed_uS * 2.0;
+        _freqLeft = (1 / period_uS) * 1E6;
+        _rpmLeft = _freqLeft / 45 * 60;
+        if (_rpmLeft > 5000) _rpmLeft = 0;
+        _mpsLeft = (WHEEL_CIRCUMFERENCE_CM / 100) * _rpmLeft / 60 / 60; 
+
+        last_uS_Left = current_uS;
+        timeout_uS_Left = last_uS_Left + SPEED_TIMEOUT;
+        lastStateLeft = stateLeft;
+    } else if (micros() > timeout_uS_Left) {
+        _freqLeft = 0;
+        _rpmLeft = 0;
+        _mpsLeft = 0;
+        last_uS_Left = micros();
+    }
+
+    if (stateRight != lastStateRight) {
+        unsigned long current_uS = micros();
+        unsigned long elapsed_uS = current_uS - last_uS_Right;
+
+        double period_uS = elapsed_uS * 2.0;
+        _freqRight = (1 / period_uS) * 1E6;
+        _rpmRight = _freqRight / 45 * 60;
+        if (_rpmRight > 5000) _rpmRight = 0;
+        _mpsRight = (WHEEL_CIRCUMFERENCE_CM / 100) * _rpmRight / 60 / 60; 
+
+        last_uS_Right = current_uS;
+        timeout_uS_Right = last_uS_Right + SPEED_TIMEOUT;
+        lastStateRight = stateRight;
+    } else if (micros() > timeout_uS_Right) {
+        _freqRight = 0;
+        _rpmRight = 0;
+        _mpsRight = 0;
+        last_uS_Right = micros();
+    }
+}
+
+// Wrap the encoder reading function for ROSBridge
+long readEncoder(int i) {
+    ReadSpeed(); // Update speed values
+
+    if (i == LEFT) return _mpsLeft;
+    else return _mpsRight;
+}
+
+// Reset encoder readings
+void resetEncoders() {
+    _mpsLeft = 0;
+    _mpsRight = 0;
+}
